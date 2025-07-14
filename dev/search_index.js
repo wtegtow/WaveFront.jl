e1d96b973b85cae577f9c36db673d07a70af9278
Documenter.jl <documenter@juliadocs.github.io>
2025-07-14 18:57:24 +0000

    build based on fc521c0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#WaveFront","page":"Home","title":"WaveFront","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WaveFront.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
